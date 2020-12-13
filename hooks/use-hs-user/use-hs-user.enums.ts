c017961ac4a655e5aed62fffded10fc52936d7a0
diegooliveira21 <qD)~&&~X:!g`q5D)KQ,8B(>
2020-12-13 14:16:08 -0300

    ### Use-hs-user 1 - Added token flow 2 - Return isLoading prop 3 - Added UseUserCommonEnum

--- hooks/use-hs-user/use-hs-user.enums.ts
+++ hooks/use-hs-user/use-hs-user.enums.ts
@@ -1,6 +1,10 @@
-export enum UseUserEnum {
+export enum UseUserSnackbarEnum {
   LoginUserFailed = 'Ops, algo deu errado. Tente novamente',
   LoginUserSuccess = 'Conta acessada com sucesso!',
   RegisterUserFailed = 'Ops, algo deu errado. Tente novamente',
   RegisterUserSuccess = 'Usuário criado com sucesso!',
+}
+
+export enum UseUserCommonEnum {
+  Token = 'USER_TOKEN',
 }
